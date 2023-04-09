8c9a6aae1b2be2ce598cf9a35909fbdae71fea4a
daxaslaliya <aslaliyadax@gmail.com>
2023-04-09 19:34:51 +0530

    created last

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#nav'
-  })
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#nav'
+//   });
+
+
+  
+var header = document.querySelector(".navbar");
+var navbarHeight = header.offsetHeight;
+var lastScrollTop = 0;
+
+window.onscroll = function() {scrollHide()};
+
+function scrollHide() {
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  console.log(st);
+  if (st > lastScrollTop) {
+    header.classList.add('hide');
+  } else {
+    header.classList.remove('hide');
+  }
+  lastScrollTop = st <= 0 ? 0 : st;
+}
