4633f65f87cd9c247bb3ca1720becbba016df810
NidhiItcode <nidhi.makdani.itcodehelp@gmail.com>
2023-04-21 20:12:57 +0530

    cmt remove

--- src/Componant/Header.js
+++ src/Componant/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
 
   return (
     <>
-      {/* <nav className="navbar navbar-expand-lg" id="nav"> 
+      {/* <nav className="navbar navbar-expand-lg" id="nav">
         <div className="container">
           <a className="navbar-brand" href="#">
           <img src={Image} alt="" width="60%"/>
@@ -72,8 +72,8 @@ const Header = () => {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"><i className="fa-solid fa-bars"></i></span>
           </button>
-          
-          <div className={`collapse ${scrollDirection === "down" && "hide"} navbar-collapse`} id="navbarNav">aa code kryo
+
+          <div className={`collapse ${scrollDirection === "down" && "hide"} navbar-collapse`} id="navbarNav">
             <ul className="navbar-nav ms-auto gap-4">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#home">Home  </a>
