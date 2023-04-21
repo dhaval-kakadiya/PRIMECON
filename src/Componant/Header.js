269580263e7540c22fa5bf68d3f22f861a37c465
Krupi221220 <satanikrupeksha@gmail.com>
2023-04-21 19:42:11 +0530

    last commit

--- src/Componant/Header.js
+++ src/Componant/Header.js
@@ -72,7 +72,8 @@ const Header = () => {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"><i className="fa-solid fa-bars"></i></span>
           </button>
-          <div className={`collapse ${scrollDirection === "down" && "hide"} navbar-collapse`} id="navbarNav">
+          
+          <div className={`collapse ${scrollDirection === "down" && "hide"} navbar-collapse`} id="navbarNav">aa code kryo
             <ul className="navbar-nav ms-auto gap-4">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#home">Home  </a>
